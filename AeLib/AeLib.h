7c1facd344c0312764164c9fbd980f087ba35dcd
njhuria <adelement@adelements-MacBook-Air-2.local>
2014-07-17 16:47:55 +0530

    new push

---
+++ AeLib/AeLib.h
@@ -0,0 +1,13 @@
+//
+//  AeLib.h
+//  AeLib
+//
+//  Created by adelement on 7/15/14.
+//  Copyright (c) 2014 adelement. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AeLib : NSObject
+
+@end
---
+++ AeLib/AeLib.m
@@ -0,0 +1,13 @@
+//
+//  AeLib.m
+//  AeLib
+//
+//  Created by adelement on 7/15/14.
+//  Copyright (c) 2014 adelement. All rights reserved.
+//
+
+#import "AeLib.h"
+
+@implementation AeLib
+
+@end
